cf6206943b21b777f477afd3135ea48b234f3885
Iben Kodehode <112859791+ibenkodehode@users.noreply.github.com>
2023-01-26 15:00:00 +0100

    Error page added

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,8 +1,12 @@
+import styles from "./Error.module.css";
 const Error = () => {
   return (
-    <>
-      <p>ERROR 404</p>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.h1}>ERROR 404</h1>
+      <h3 className={styles.h3}>
+        Gå tilbake til <span className={styles.span}>forsiden</span>
+      </h3>
+    </div>
   );
 };
 
