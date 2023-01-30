53fd4df4e595e48b82bfba5c8f3aa0a7836e0647
Iben Kodehode <112859791+ibenkodehode@users.noreply.github.com>
2023-01-30 14:16:21 +0100

    Added React Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,20 @@
 // Libraries
-import { useRef } from "react";
-// import { gsap } from "gsap";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 // Styles
 import "./App.css";
 
 // Pages
-import Landing from "./pages/Landing/Landing";
-import Projects from "./pages/Projects/Projects";
-import About from "./pages/About/About";
-
-// Icons
-import { AiOutlineArrowUp } from "react-icons/ai";
+import Home from "./pages/Home/Home";
 
 function App() {
-  const landingRef = useRef(null);
-  const projectsRef = useRef(null);
-  const aboutRef = useRef(null);
-  const topRef = useRef(null);
-
-  const handleClick = (ref) => {
-    ref.current.scrollIntoView({ behavior: "smooth", block: "start" });
-  };
-
   return (
-    <div className="app" ref={topRef}>
-      <header>
-        <nav>
-          <button onClick={() => handleClick(landingRef)}>Landing</button>
-          <button onClick={() => handleClick(projectsRef)}>Projects</button>
-          <button onClick={() => handleClick(aboutRef)}>About</button>
-        </nav>
-      </header>
-
-      <main>
-        <div ref={landingRef}>
-          <Landing />
-        </div>
-        <button onClick={() => handleClick(topRef)} className="top-btn">
-          <AiOutlineArrowUp />
-        </button>
-        <div ref={projectsRef}>
-          <Projects />
-        </div>
-        <div ref={aboutRef}>
-          <About />
-        </div>
-      </main>
+    <div className="app">
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Landing from "../Landing/Landing";
+import Projects from "../Projects/Projects";
+import About from "../About/About";
+
+const Home = () => {
+  return (
+    <>
+      <Landing />
+      <Projects />
+      <About />
+    </>
+  );
+};
+
+export default Home;
