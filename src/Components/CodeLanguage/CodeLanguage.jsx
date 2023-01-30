994eb01f11931e57f19eb8331febd1354fa1afa3
Iben Kodehode <112859791+ibenkodehode@users.noreply.github.com>
2023-01-30 13:16:16 +0100

    Added code icons

---
+++ src/Components/CodeLanguage/CodeLanguage.jsx
@@ -0,0 +1,45 @@
+//Styles
+import styles from "./CodeLanguage.module.css";
+
+// Icons
+import { DiGithubBadge } from "react-icons/di"; //github
+import { DiGit } from "react-icons/di"; //git
+import { DiJavascript1 } from "react-icons/di"; //js
+import { DiReact } from "react-icons/di"; //react
+import { DiVisualstudio } from "react-icons/di"; //vs code
+import { SiCss3 } from "react-icons/si"; //css
+import { SiFigma } from "react-icons/si"; //figma
+import { SiHtml5 } from "react-icons/si"; //html5
+import { SiVite } from "react-icons/si"; //vite
+import { SiTailwindcss } from "react-icons/si"; //tailwind
+import { DiNpm } from "react-icons/di"; //npm
+
+const Icons = () => {
+  return (
+    <div>
+      <div className={styles.container}>
+        <h2 className={styles.headline}>Verktøy jeg bruker daglig </h2>
+        <div className={styles.wrap}>
+          <DiGithubBadge className={styles.icon} />
+          <DiJavascript1 className={styles.icon} />
+          <SiCss3 className={styles.icon} />
+          <SiFigma className={styles.icon} />
+          <SiHtml5 className={styles.icon} />
+          <SiVite className={styles.icon} />
+          <DiVisualstudio className={styles.icon} />
+          <DiReact className={styles.icon} />
+        </div>
+      </div>
+      <div className={styles.container}>
+        <h3 className={styles.headlines}>Verktøy jeg er kjent med</h3>
+        <div className={styles.wrapped}>
+          <DiGit className={styles.icons} />
+          <SiTailwindcss className={styles.icons} />
+          <DiNpm className={styles.icons} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Icons;
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,19 +1,28 @@
+// styles
 import styles from "./Landing.module.css";
+
+//Components
+import Icons from "../../Components/CodeLanguage/CodeLanguage";
 
 const Landing = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.h1}>Iben</h1>
-      <h1 className={styles.etternavn}>Kodehode</h1>
-      <h5 className={styles.h5}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-        <span className={styles.span}>
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat.
-        </span>
-      </h5>
-    </div>
+    <section className={styles.container}>
+      <article className={styles.intro}>
+        <h1 className={styles.h1}>Iben</h1>
+        <h1 className={styles.etternavn}>Kodehode</h1>
+        <p className={styles.h5}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <span className={styles.span}>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </span>
+        </p>
+      </article>
+      <article className={styles.icon}>
+        <Icons />
+      </article>
+    </section>
   );
 };
 
