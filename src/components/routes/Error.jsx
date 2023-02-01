c57afccc8ce802947d6ae22e97ac1da1e1990199
Iben Kodehode <112859791+ibenkodehode@users.noreply.github.com>
2023-02-01 10:06:18 +0100

    Fixed link back to homepage

--- src/components/routes/Error.jsx
+++ src/components/routes/Error.jsx
@@ -1,15 +1,24 @@
+// libraries
+import { Link } from "react-router-dom";
+
 // styles
 import styles from "../styles/Error.module.css";
 
 const Error = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.head}>ERROR 404</h1>
-      <h3 className={styles.h3}>
-        Gå tilbake til <span className={styles.link}>forsiden</span>
+      <h1 className={styles.headline}>ERROR 404</h1>
+      <h3>
+        Gå tilbake til
+        <Link to="/portfolio/" className={styles.link}>
+          &nbsp;forsiden
+        </Link>
       </h3>
     </div>
   );
 };
 
 export default Error;
+
+// to do /can do
+//add link to close btn on container
