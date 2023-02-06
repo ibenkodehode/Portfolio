d95ac28023705e44903a947eb2e1762b11bcd9f0
Iben Kodehode <112859791+ibenkodehode@users.noreply.github.com>
2023-02-06 14:11:14 +0100

    Custom hover color added

--- src/components/elements/Project.jsx
+++ src/components/elements/Project.jsx
@@ -1,16 +1,15 @@
 import styles from "../styles/Project.module.css";
 
-const Project = () => {
+const Project = (props) => {
   return (
-    <div className={styles.card}>
-      <div className={styles.img}>img</div>
-      {/* <img src="" alt="" /> */}
+    <div className={styles.card} style={{ "--hover_color": props.hoverColor }}>
+      <img src={props.src} alt={props.alt} className={styles.img} />
       <div className={styles.flex}>
         <div>
-          <h3 className={styles.h3}>Prosjekt</h3>
-          <p className={styles.p}>Litt om prosj</p>
+          <h3 className={styles.h3}>{props.title}</h3>
+          <p className={styles.p}>{props.link}</p>
         </div>
-        <div>Icon</div>
+        <div>{props.icon}</div>
       </div>
     </div>
   );
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,14 +1,32 @@
+// components
 import Project from "../elements/Project";
+
+// styles
 import styles from "../styles/Projects.module.css";
+
+//images
+import soundboard from "../../img/soundboard.png";
+import generatePassword from "../../img/passwordgenerator.png";
 
 const Projects = () => {
   return (
     <section id="prosjekter">
       <h3>Prosjekter</h3>
       <div className={styles.flex}>
-        <Project />
-        <Project />
-        <Project />
+        <Project
+          src={soundboard}
+          alt="Screenshot of a project displaying a pink soundboard"
+          title="Soundboard"
+          icon="test"
+          hoverColor="#E7C3D8"
+        />
+        <Project
+          src={generatePassword}
+          alt="Screenshot showing a webpage generating a random password"
+          title="Generate random password"
+          icon="test"
+          hoverColor="#00B987"
+        />
       </div>
     </section>
   );
